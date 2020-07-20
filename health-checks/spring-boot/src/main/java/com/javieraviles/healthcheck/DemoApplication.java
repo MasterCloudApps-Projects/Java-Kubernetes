78ed8f49a0bf841151644b2ba19b7c5be5b86dbf
Javier Aviles <avileslopez.javier@gmail.com>
2020-07-21 01:36:01 +0200

    Custom health check for generic service, db health check included already

--- health-checks/spring-boot/src/main/java/com/javieraviles/healthcheck/DemoApplication.java
+++ health-checks/spring-boot/src/main/java/com/javieraviles/healthcheck/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.javieraviles.healthcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- health-checks/spring-boot/src/main/java/com/javieraviles/healthcheck/health/CustomHealthCheck.java
+++ health-checks/spring-boot/src/main/java/com/javieraviles/healthcheck/health/CustomHealthCheck.java
@@ -1,29 +1,28 @@
-package com.example.demo.health;
+package com.javieraviles.healthcheck.health;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DatabaseConnectionHealthCheck implements HealthIndicator {
+public class CustomHealthCheck implements HealthIndicator {
 
-	private boolean isDatabaseUp = true;
-	private String serviceName = "Database-connection";
+	private boolean isCustomServiceUp = true;
+	private String serviceName = "Custom-service";
 
 	@Override
 	public Health health() {
 		try {
-			simulateDatabaseConnectionVerification();
+			simulateCustomServiceConnectionVerification();
 			return Health.up().withDetail(serviceName, "Available").build();
 		} catch (IllegalStateException e) {
-			// cannot access the database
 			return Health.down().withDetail(serviceName, e.getMessage()).build();
 		}
 	}
 
-	private void simulateDatabaseConnectionVerification() {
-		if (!isDatabaseUp) {
-			throw new IllegalStateException("Cannot contact database");
+	private void simulateCustomServiceConnectionVerification() {
+		if (!isCustomServiceUp) {
+			throw new IllegalStateException("Cannot reach custom service");
 		}
 	}
 
